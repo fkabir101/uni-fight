ef732ace7691d922ce7e70248b06453f323b2c3b
Gregg Hosie <gregg.hosie@gmail.com>
2019-02-27 10:08:58 -0500

    eventcards with routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Form from './components/form/form'
 import Header from "./compnents/header/headerMaine"
+import MainPage from "./components/mainPage/mainPage.js";
+
+
 
 class App extends Component {
   render() {
@@ -11,6 +14,7 @@ class App extends Component {
           <Header />
           <Switch>
             <Route exact path="/create" component={Form}/>
+            <Route exact path="/" component={MainPage}/>
           </Switch>
         </div>
       </Router>
