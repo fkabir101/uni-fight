66e3df4cceed58f1dac7d67820b719f68ac8b26f
Gregg Hosie <gregg.hosie@gmail.com>
2019-03-05 18:31:28 -0500

    axios call routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,11 +2,36 @@ const mongoose = require("mongoose");
 const db = require("../models")
 
 module.exports = {
-  createEvent : function(req, res){
+  createEvent: function (req, res) {
     db.Events
       .create(req.body)
-      .then(dbModel => res.json({dbModel}))
+      .then(dbModel => res.json({ dbModel }))
       .catch(err => res.status(422).json(err));
-   // res.json(req.body);
+  },
+  findAll: function (req, res) {
+    db.Events
+      .find(req.query)
+      .sort({ start: 1 })
+      .then(dbModel => res.json({ dbModel }))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Events
+      .findById(req.params.id)
+      .then(dbModel => res.json({ dbModel }))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Events
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json({ dbModel }))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function (req, res) {
+    db.Events
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json({ dbModel }))
+      .catch(err => res.status(422).json(err));
   }
-}
+};
--- routes/api/events.js
+++ routes/api/events.js
@@ -4,4 +4,9 @@ const eventController = require("../../controllers/eventController");
 router.route("/create")
   .post(eventController.createEvent);
 
+router.route("/:id")
+  .get(eventController.findById)
+  .put(eventController.update)
+  .delete(eventController.remove);
+
 module.exports = router
