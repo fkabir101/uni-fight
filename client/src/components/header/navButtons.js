4e5dea095f0c66cb3617ebf092792d81fbae24f5
Gregg Hosie <gregg.hosie@gmail.com>
2019-03-08 12:24:40 -0500

    merged

--- client/src/components/header/navButtons.js
+++ client/src/components/header/navButtons.js
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import CssSwitcher from "../themeswitcher/cssSwitcher";
-
-const localUser = sessionStorage.getItem("userName");
-//const localUser = "admin";
-
-function RenderNavbar() {
-  console.log("localUser: " + localUser);
-
-  if (localUser === "null" || localUser === null || localUser === undefined) {
-    console.log("User is logged out");
-    return (
-      <div>
-        <ul className="nav justify-content-end">
-        <li className="nav-item">
-            <a className="btn btn-secondary" href="/login">Login</a>
-          </li>
-          <li className="nav-item">
-            <a className="btn btn-secondary" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="btn btn-secondary" href="/events">View Events</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-  else {
-    console.log("User is logged in");
-    return (
-      <div>
-        <ul className="nav justify-content-end">
-        <li>
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User</button>
-
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" id="userLink" href="/user">Your Page</a>
-              <a className="dropdown-item" id="createLink" href="/create">Create Event</a>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item" id="logoutLink">Logout</button>
-            </div>
-          </div>
-          </li>
-          <li className="nav-item">
-            <a className="btn btn-secondary" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="btn btn-secondary" href="/events">View Events</a>
-          </li>
-        </ul>
-      </div>
-    )
-=======
 
 
 import React, { Component } from "react";
@@ -131,7 +76,6 @@ class RenderNavbar extends Component {
         </div>
       )
     }
->>>>>>> 12c7c920d6dca275cfd7d1f4768d537f35ba5174
   }
 }
 
