acb56b2f92d888e3e3cad17c666262c0be0ead29
Griff <griffal@comcast.net>
2019-03-28 13:07:32 -0400

    sticky footer

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -66,6 +66,7 @@ module.exports = {
             });
           });
         }//if dbModel === null
+        
         // else {
         //   alert("Username is taken.  Please try again.");
         // }
