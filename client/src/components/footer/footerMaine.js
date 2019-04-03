45a5c2ccff461394ee8a343acc7a114793493ff2
Griff <griffal@comcast.net>
2019-04-03 19:29:39 -0400

    errors and footer stay

--- client/src/components/footer/footerMaine.js
+++ client/src/components/footer/footerMaine.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <nav className="navbar navbar-dark bg-dark footer navbar-fixed-bottom" id="footer">
   <span className="text-center navbar-text" id="footText">
-  <i className="far fa-copyright"></i> This is a Real Copyright!  Don't Copy 'Cause I Said So!
+  <i className="far fa-copyright"></i> Copyright Unify 2019
   </span>
 </nav>
   )
